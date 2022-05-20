de63fca87d8c846b95d31ee26449a631b70ad77c
alec <abacqu0716@gmail.com>
2022-05-20 18:51:18 -0500

    index started

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const Pokemon = require('../models/pokemon.js');
+// const Pokemon = require('./models/pokemon');
+const Pokemon = require('./models/pokemon');
 
 
 const port = 3000;
@@ -8,13 +9,23 @@ const port = 3000;
 
 // INDEX
 app.get('/', (req, res) => {
-    res.render('index.ejs', { data: Pokemon });
+    // res.render('index.ejs', { pokemon: Pokemon });
+    res.render('index.ejs', { pokemon: Pokemon});
 });
 
+// NEW
+
+// DELETE
+
+// UPDATE
+
+// CREATE
+
+// EDIT
 
 // SHOW
 app.get('/:id', (req, res) => {
-    res.render('show.ejs', { data: Pokemon[req.params.id] });
+    res.render('show.ejs', { pokemon: Pokemon[req.params.id] });
 });
 
 
